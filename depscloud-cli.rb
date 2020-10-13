df35b0018ab2726ea28f72ca127ccb9a329051af
goreleaserbot <mjpitz@deps.cloud>
2020-10-13 09:21:18 -0500

    Brew formula update for depscloud version v0.2.29

--- depscloud-cli.rb
+++ depscloud-cli.rb
@@ -2,24 +2,24 @@
 class DepscloudCli < Formula
   desc "Command line interface to the deps.cloud API"
   homepage "https://deps.cloud/"
-  version "0.2.28"
+  version "0.2.29"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/depscloud/depscloud/releases/download/v0.2.28/deps_0.2.28_darwin_amd64.tar.gz"
-    sha256 "cb384784479142374f66e382b029735e70724ed9548b26a79a3929ec7cf75b12"
+    url "https://github.com/depscloud/depscloud/releases/download/v0.2.29/deps_0.2.29_darwin_amd64.tar.gz"
+    sha256 "606b54d059a0895e11bccc41cf5d3123cb99958715d2618bae95ccc2a2a73973"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/depscloud/depscloud/releases/download/v0.2.28/deps_0.2.28_linux_amd64.tar.gz"
-      sha256 "7a67262b74cc212840e95579ea6fd4d0db0e3c28fdaaa6ca7e6fa2db52eaa72d"
+      url "https://github.com/depscloud/depscloud/releases/download/v0.2.29/deps_0.2.29_linux_amd64.tar.gz"
+      sha256 "6a3f8caad2bc9f734fdd256d7d26f57da6f77437716fa1da8b8e07ded940fbf0"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/depscloud/depscloud/releases/download/v0.2.28/deps_0.2.28_linux_arm64.tar.gz"
-        sha256 "ecc50456529066eca5a3d4179afe24ca1709d6d755533509f6466c412a64d24c"
+        url "https://github.com/depscloud/depscloud/releases/download/v0.2.29/deps_0.2.29_linux_arm64.tar.gz"
+        sha256 "a1e1074cf95390bcc9278c75df343159bc8bbe46f5f18feaab3bb3dc055c7cad"
       else
-        url "https://github.com/depscloud/depscloud/releases/download/v0.2.28/deps_0.2.28_linux_armv7.tar.gz"
-        sha256 "d5e9b4f1496c90a6b4939a3a0654b3330d1c0705d6e43a2a3a5f771dfd152ad4"
+        url "https://github.com/depscloud/depscloud/releases/download/v0.2.29/deps_0.2.29_linux_armv7.tar.gz"
+        sha256 "4854b2f761dfa133170df2846fc4ee4f61f7588212276422d3310e9fe2deda3e"
       end
     end
   end
